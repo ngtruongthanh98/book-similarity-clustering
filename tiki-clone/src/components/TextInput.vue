1c4742e4bd5eb0ef6401344365ad84dd6f650930
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2023-05-01 21:59:00 +0700

    feat: Install vuestic-ui and sass

---
+++ tiki-clone/src/components/TextInput.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  label: {
+    type: String,
+    default: ''
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  }
+})
+
+const value = ref('')
+</script>
+
+<template>
+  <div style="max-width: 300px;">
+    <va-input
+      v-model="value"
+      class="mb-6"
+      :label="label"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- tiki-clone/vite.config.js
+++ tiki-clone/vite.config.js
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/_animations.scss";
+          @import "./src/styles/_variables.scss";
+          @import "./src/styles/_mixins.scss";
+          @import "./src/styles/_helpers.scss";
+        `
+      }
+    }
+  }
 })
