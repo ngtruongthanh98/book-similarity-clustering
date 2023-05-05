bd550bf2ccc40dc37125f12e849322a2d1965f89
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2023-05-06 06:19:31 +0700

    feat: Breadcrumb

--- tiki-clone/src/views/TikiBookstore/index.vue
+++ tiki-clone/src/views/TikiBookstore/index.vue
@@ -1,16 +1,64 @@
 <template>
   <div class="tiki-bookstore-page">
-    Tiki Bookstore
+    <div class="breadcrumb-wrapper">
+      <div class="breadcrumb-container">
+        <va-breadcrumbs
+          separator=">"
+          class="custom-breadcrumb"
+        >
+          <va-breadcrumbs-item label="Trang chủ" class="homepage-breadcrumb" @click="onClickGotoHome"/>
+          <va-breadcrumbs-item label="Nhà sách Tiki" class="bookstore-breadcrumb" />
+        </va-breadcrumbs>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
-
+  methods: {
+    onClickGotoHome() {
+      this.$router.push({ name: 'home' });
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
   .tiki-bookstore-page {
-    background-color: #f5f5fa;
-    // height: calc(100vh - 80px - 8px);
+    .breadcrumb-wrapper {
+      background-color: rgb(245, 245, 250);
+
+      .breadcrumb-container {
+        width: 1270px;
+        height: 40px;
+        padding-left: 15px;
+        padding-right: 15px;
+        margin-right: auto;
+        margin-left: auto;
+
+        .custom-breadcrumb {
+          height: 100%;
+
+          color: rgb(128, 128, 137);
+          font-size: 14px;
+          line-height: 20px;
+          font-weight: 300;
+          white-space: nowrap;
+
+          .homepage-breadcrumb {
+            margin-right: 5.5px;
+
+            &:hover {
+              text-decoration: underline;
+              cursor: pointer;
+            }
+          }
+
+          .bookstore-breadcrumb {
+            margin-left: 8.5px;
+            color: rgb(56, 56, 61);
+          }
+        }
+      }
+    }
   }
 </style>
