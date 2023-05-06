58b50578c7c5e62f538b19681456fa1510700658
ThanhNT26 <ngtruongthanh98@gmail.com>
2023-05-06 17:09:25 +0700

    feat: Handle routing to book details

--- tiki-clone/src/views/Homepage/TodayRecommend/index.vue
+++ tiki-clone/src/views/Homepage/TodayRecommend/index.vue
@@ -15,7 +15,7 @@
     <div class="today-recommend__body">
       <div class="content">
 
-        <div v-for="(ele, index) in PRODUCT_LIST" :key="index" class="product-item">
+        <div v-for="(ele, index) in PRODUCT_LIST" :key="index" class="product-item" @click="onClickProductDetail(ele.productId)">
           <div class="styled-item">
             <div class="thumbnail">
               <img class="official-image" src="https://salt.tikicdn.com/ts/upload/5d/4c/f7/0261315e75127c2ff73efd7a1f1ffdf2.png" alt="Official" width="68" height="14">
@@ -118,7 +118,8 @@ export default {
           price: '82.000',
           discount: '25',
           backAmount: '2',
-          backAmountPrice: '408'
+          backAmountPrice: '408',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/f5/52/80/675e31a670afc560e7b0e46c0b65fb4f.png.webp',
@@ -128,7 +129,8 @@ export default {
           price: '27.450.000',
           discount: '19',
           backAmount: '686',
-          backAmountPrice: '136k'
+          backAmountPrice: '136k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/aa/24/e0/57914989351786ef9dfe027046ba4626.jpg.webp',
@@ -138,7 +140,8 @@ export default {
           price: '10.610.000',
           discount: '18',
           backAmount: '265',
-          backAmountPrice: '53k'
+          backAmountPrice: '53k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/0c/dd/e7/4be0add110579c0ce57e53fba16e656e.png.webp',
@@ -148,7 +151,8 @@ export default {
           price: '30.990.000',
           discount: '13',
           backAmount: '631',
-          backAmountPrice: '126'
+          backAmountPrice: '126',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/a2/38/6c/ce008c63f4ac771550439da44f5f8ee8.png.webp',
@@ -158,7 +162,8 @@ export default {
           price: '16.990.000',
           discount: '32',
           backAmount: '425',
-          backAmountPrice: '84k'
+          backAmountPrice: '84k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/0c/28/ef/8c4a83ffeb29e5faa120a80ad05ba178.jpg.webp',
@@ -168,7 +173,8 @@ export default {
           price: '3.988.000',
           discount: '20',
           backAmount: '2100',
-          backAmountPrice: '20k'
+          backAmountPrice: '20k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/45/3b/fc/aa81d0a534b45706ae1eee1e344e80d9.jpg.webp',
@@ -178,7 +184,8 @@ export default {
           price: '51.350',
           discount: '35',
           backAmount: '1',
-          backAmountPrice: '255'
+          backAmountPrice: '255',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/eb/76/de/a0e87ef33a285d22f4659374a1f1f177.jpg.webp',
@@ -188,7 +195,8 @@ export default {
           price: '230.800',
           discount: '40',
           backAmount: '6',
-          backAmountPrice: '1k'
+          backAmountPrice: '1k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/29/43/11/71c0b521f23ae08bc4dc6ed24dbc98a7.jpg.webp',
@@ -198,7 +206,8 @@ export default {
           price: '284.050',
           discount: '5',
           backAmount: '56',
-          backAmountPrice: '11k'
+          backAmountPrice: '11k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/54/ff/25/bfdf0febe11a28eaa7cd3fa735a82c49.png.webp',
@@ -208,7 +217,8 @@ export default {
           price: '7.090.000',
           discount: '35',
           backAmount: '177',
-          backAmountPrice: '35k'
+          backAmountPrice: '35k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/94/df/44/1efb32bd47cbe070887e460a3ec8cb83.jpg.webp',
@@ -218,7 +228,8 @@ export default {
           price: '195.000',
           discount: '7',
           backAmount: '5',
-          backAmountPrice: '969'
+          backAmountPrice: '969',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/c0/be/5a/67433e81553e46f4351ac451382c7ec0.png.webp',
@@ -228,7 +239,8 @@ export default {
           price: '459.000',
           discount: '6',
           backAmount: '11',
-          backAmountPrice: '2k'
+          backAmountPrice: '2k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/da/fe/dc/cb8682708ffee62939112775f1538fe5.jpg.webp',
@@ -238,7 +250,8 @@ export default {
           price: '120.000',
           discount: '10',
           backAmount: '3',
-          backAmountPrice: '596'
+          backAmountPrice: '596',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/2d/59/84/1ec9c569287d0532dd40cecf4094071f.jpg.webp',
@@ -248,7 +261,8 @@ export default {
           price: '219.000',
           discount: '7',
           backAmount: '5',
-          backAmountPrice: '1k'
+          backAmountPrice: '1k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/81/ee/a6/831809b5596fdce0ae16033df2522d30.jpg.webp',
@@ -258,7 +272,8 @@ export default {
           price: '179.000',
           discount: '19',
           backAmount: '4',
-          backAmountPrice: '890'
+          backAmountPrice: '890',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/98/07/0a/c1086b930db321b12e09588910fbead2.jpg.webp',
@@ -268,7 +283,8 @@ export default {
           price: '105.000',
           discount: '36',
           backAmount: '21',
-          backAmountPrice: '4k'
+          backAmountPrice: '4k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/5e/18/24/2a6154ba08df6ce6161c13f4303fa19e.jpg.webp',
@@ -278,7 +294,8 @@ export default {
           price: '70.200',
           discount: '35',
           backAmount: '2',
-          backAmountPrice: '349'
+          backAmountPrice: '349',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/b3/d0/de/f9f879a32d4ba31bf5084b05e6fc7889.jpg.webp',
@@ -288,7 +305,8 @@ export default {
           price: '148.000',
           discount: '73',
           backAmount: '29',
-          backAmountPrice: '6k'
+          backAmountPrice: '6k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/49/75/99/2219ae33da6fb87ec3231dae0e752b3a.jpg.webp',
@@ -298,7 +316,8 @@ export default {
           price: '12.490.000',
           discount: '17',
           backAmount: '312',
-          backAmountPrice: '62k'
+          backAmountPrice: '62k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/30/ee/5a/e2aed009bb558b5d2cfbbe157b428ba4.jpg.webp',
@@ -308,7 +327,8 @@ export default {
           price: '134.000',
           discount: '20',
           backAmount: '3',
-          backAmountPrice: '666'
+          backAmountPrice: '666',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/64/1e/1a/60309a84a30a722425e56a3ad52eb9a0.png.webp',
@@ -318,7 +338,8 @@ export default {
           price: '5.478.000',
           discount: '9',
           backAmount: '695',
-          backAmountPrice: '138k'
+          backAmountPrice: '138k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/2c/3d/85/3fcc90a1300a79922ec58ef22afae47a.jpg.webp',
@@ -328,7 +349,8 @@ export default {
           price: '2.672.000',
           discount: '31',
           backAmount: '67',
-          backAmountPrice: '13k'
+          backAmountPrice: '13k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/43/dd/97/258137d331bf9359c814034a52388ff4.jpg.webp',
@@ -338,7 +360,8 @@ export default {
           price: '2.800.000',
           discount: '5',
           backAmount: '70',
-          backAmountPrice: '14k'
+          backAmountPrice: '14k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/22/cb/a9/524a27dcd45e8a13ae6eecb3dfacba7c.jpg.webp',
@@ -348,7 +371,8 @@ export default {
           price: '2.800.000',
           discount: '5',
           backAmount: '2',
-          backAmountPrice: '328'
+          backAmountPrice: '328',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/22/cb/a9/524a27dcd45e8a13ae6eecb3dfacba7c.jpg.webp',
@@ -358,7 +382,8 @@ export default {
           price: '66.000',
           discount: '33',
           backAmount: '2',
-          backAmountPrice: '328'
+          backAmountPrice: '328',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/9c/1b/57/ffd2d24eefe918338dd64039fc9be957.jpg.webp',
@@ -368,7 +393,8 @@ export default {
           price: '90.000',
           discount: '15',
           backAmount: '2',
-          backAmountPrice: '447'
+          backAmountPrice: '447',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/ec/45/40/d779c7b58bf0e74d45972e8a359e1d4e.png.webp',
@@ -378,7 +404,8 @@ export default {
           price: '19.250.000',
           discount: '23',
           backAmount: '481',
-          backAmountPrice: '96k'
+          backAmountPrice: '96k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/1a/1d/0a/f280cf83e7be6c502c54fe2f1b3a8b12.jpg.webp',
@@ -388,7 +415,8 @@ export default {
           price: '3.990.000',
           discount: '17',
           backAmount: '100',
-          backAmountPrice: '20k'
+          backAmountPrice: '20k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/42/0f/92/2f5a875401334239950b277660ccc79f.jpg.webp',
@@ -398,7 +426,8 @@ export default {
           price: '14.190.000',
           discount: '25',
           backAmount: '350',
-          backAmountPrice: '70k'
+          backAmountPrice: '70k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/c0/f7/db/e98998e0d92de8981d1d8485ce96a3f8.jpg.webp',
@@ -408,7 +437,8 @@ export default {
           price: '6.370.000',
           discount: '20',
           backAmount: '159',
-          backAmountPrice: '32k'
+          backAmountPrice: '32k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/92/33/e2/5d9947f5ccb5481113c97aa6a5fab584.jpg.webp',
@@ -418,7 +448,8 @@ export default {
           price: '249.000',
           discount: '50',
           backAmount: '6',
-          backAmountPrice: '1k'
+          backAmountPrice: '1k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/e0/c9/78/e028f9346e1922b0aadc750f272609c3.jpg.webp',
@@ -428,7 +459,8 @@ export default {
           price: '14.990.000',
           discount: '12',
           backAmount: '375',
-          backAmountPrice: '75k'
+          backAmountPrice: '75k',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/65/78/e7/2eacb9f887fc28779b15cf9ab6d0bf79.jpg.webp',
@@ -438,7 +470,8 @@ export default {
           price: '60.200',
           discount: '30',
           backAmount: '2',
-          backAmountPrice: '299'
+          backAmountPrice: '299',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/7f/30/16/51d47c3c68c911d97350eb2374d6c408.jpg.webp',
@@ -448,7 +481,8 @@ export default {
           price: '58.000',
           discount: '39',
           backAmount: '1',
-          backAmountPrice: '288'
+          backAmountPrice: '288',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/16/8f/ce/5131b0d2dab7e3a30e6fc3778852ce36.jpg.webp',
@@ -458,7 +492,8 @@ export default {
           price: '134.000',
           discount: '20',
           backAmount: '3',
-          backAmountPrice: '666'
+          backAmountPrice: '666',
+          productId: '1234'
         },
         {
           imageUrl: 'https://salt.tikicdn.com/cache/280x280/ts/product/81/bc/7b/e816f29fc6e20711f8dc9da9bde3cfa0.jpg.webp',
@@ -468,9 +503,15 @@ export default {
           price: '88.000',
           discount: '26',
           backAmount: '2',
-          backAmountPrice: '437'
+          backAmountPrice: '437',
+          productId: '1234'
         }
       ]
+    }
+  },
+  methods: {
+    onClickProductDetail(productId) {
+      this.$router.push(`/nha-sach-tiki/${productId}`);
     }
   }
 }
