fd45229783d787d74838f7498d4f7938472f5f47
ThanhNT26 <ngtruongthanh98@gmail.com>
2023-05-06 15:13:32 +0700

    feat: Create book details route

--- tiki-clone/src/router/index.js
+++ tiki-clone/src/router/index.js
@@ -1,24 +1,30 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from '../views/Homepage/index.vue'
-import TikiBookstore from '../views/TikiBookstore/index.vue'
+import Home from '../views/Homepage/index.vue';
+import TikiBookstore from '../views/TikiBookstore/index.vue';
+import BookDetails from '../views/BookDetails/index.vue';
 
 const routes = [
-    {
-        path: '/',
-        name: "home",
-        component: Home
-    },
-    {
-        path: '/nha-sach-tiki',
-        name: 'tiki-bookstore',
-        component: TikiBookstore
-    }
-]
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/nha-sach-tiki',
+    name: 'tiki-bookstore',
+    component: TikiBookstore
+  },
+  {
+    path: '/nha-sach-tiki/:productId',
+    name: 'book-details',
+    component: BookDetails
+  },
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: routes
-})
+  history: createWebHistory(),
+  routes: routes
+});
 
-export default router
+export default router;
